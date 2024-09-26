2cdd8927424a18df0e0f6a3adc4a3eabd3f60520
Kiki <marianecanhe@gmail.com>
2024-09-26 13:55:44 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/mariaNcanghe/',
   plugins: [react()],
-  base: '/Maria-Ncanghe/',
 })
